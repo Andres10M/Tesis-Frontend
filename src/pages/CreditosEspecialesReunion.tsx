d8eded61e7020588877f3cfd84916cc8ab273e25
Andres10M <pasari@sudamericano.edu.ec>
2026-03-02 20:10:35 -0500

    holatttttYoty

--- src/pages/CreditosEspecialesReunion.tsx
+++ src/pages/CreditosEspecialesReunion.tsx
@@ -18,7 +18,7 @@ interface Fila {
   fijo?: boolean;
 }
 
-const API = "https://72.60.10.162:4000";
+const API = "http://72.60.10.162:4000/api";
 
 export default function CreditosEspecialesReunion() {
   const navigate = useNavigate();
